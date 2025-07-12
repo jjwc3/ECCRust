c3799713ea745fec456959a6c64f8f0398ef13e3
jjwc3 <jjwjjw2007@naver.com>
2025-07-12 11:29:08 +0900

    250712

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Encryption
+// use tokio::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file_original_path = String::new();
+    println!("Enter the File Path: ");
+    std::io::stdin().read_line(&mut file_original_path).unwrap();
+    println!("Path: {}", file_original_path);
+
+    // let file_path =
 }
+
+
