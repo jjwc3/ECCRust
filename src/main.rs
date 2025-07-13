59d742e730c3d082145b78d93bd94b6e2e9eb138
jjwc3 <jjwjjw2007@naver.com>
2025-07-13 18:05:29 +0900

    250713

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 // Encryption
 use std::fs;
 use std::io;
-use aes::Aes256;
-use cipher::{KeyIvInit, block_padding::Pkcs7, BlockEncryptMut};
-use cbc::Encryptor;
+use std::path::Path;
 use rand::{rngs::OsRng, TryRngCore};
 use hex;
-use std::path::Path;
-
+use cipher::{KeyIvInit, block_padding::Pkcs7, BlockEncryptMut};
+use aes::Aes256;
+use cbc::Encryptor;
 
 const KEY_SIZE: usize = 32; // 대칭키 크기
 const BLOCK_SIZE: usize = 16; // AES Block 크기
@@ -24,29 +23,27 @@ fn write_bytes_to_file_sync(path: &str, data: &[u8]) -> Result<(), io::Error> {
 
 // main 실행 함수. 코드가 실행되면 이 함수가 실행됨.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    aes_encription()?;
+    let aes_key = aes_encryption()?;
+    ecc_encryption(aes_key)?;
     Ok(())
 }
 
 // AES-256 암호화 함수. 파일 데이터를 암호화함.
-fn aes_encription() -> Result<(), Box<dyn std::error::Error>> {
+fn aes_encryption() -> Result<[u8; KEY_SIZE], Box<dyn std::error::Error>> {
 
     // 비밀 키를 생성함.
     let mut key_bytes = [0u8; KEY_SIZE];
     OsRng.try_fill_bytes(&mut key_bytes)?;
-    println!("비밀 키: {}", hex::encode(&key_bytes));
 
     // 초기화 벡터를 생성함.
     let mut iv_bytes = [0u8; BLOCK_SIZE];
     OsRng.try_fill_bytes(&mut iv_bytes)?;
-    // println!("{}", hex::encode(&iv_bytes));
 
     let mut file_temp_path = String::new();
     println!("Enter the File Path: ");
     io::stdin().read_line(&mut file_temp_path).unwrap();
     let file_original_path_str = file_temp_path.trim().trim_matches('\'');
     let file_original_path = Path::new(file_original_path_str);
-    // println!("Path: {}", file_original_path);
     let plain_bytes = read_file_to_bytes_sync(file_original_path_str)?;
 
     let buffer_len = plain_bytes.len() + BLOCK_SIZE;
@@ -58,23 +55,29 @@ fn aes_encription() -> Result<(), Box<dyn std::error::Error>> {
     let ciphertext_bytes = cipher.encrypt_padded_mut::<Pkcs7>(&mut buffer, plain_bytes.len())
         .map_err(|e| format!("Encryption Error: {:?}", e))?;
 
-    println!("\n생성된 암호문 (Hex): {}", hex::encode(&ciphertext_bytes)); // (1)
+    println!("\n생성된 암호문 (Hex, without IV): {}", hex::encode(ciphertext_bytes));
     println!("암호문 길이 (패딩 포함): {} 바이트", ciphertext_bytes.len());
+    println!("비밀 키: {}", hex::encode(&key_bytes));
+    println!("IV: {}", hex::encode(&iv_bytes));
 
     let mut encrypted_file_content = iv_bytes.to_vec();
-    encrypted_file_content.extend_from_slice(ciphertext_bytes); // 만약 (1)에서 &가 빠지면, hex::encode로 value가 이동하므로 이 line에서 해당 변수를 사용할 수 없음. 소유권을 제대로 이해할 수 있는 코드임.
+    encrypted_file_content.extend_from_slice(ciphertext_bytes);
 
-    println!("{}", hex::encode(&encrypted_file_content));
+    println!("IV + 암호문: {}", hex::encode(&encrypted_file_content));
 
     let output_file_name = format!("{}.aes", file_original_path.file_name().unwrap().to_string_lossy());
     let output_file_path = file_original_path.with_file_name(output_file_name);
 
     write_bytes_to_file_sync(output_file_path.to_str().unwrap(), &encrypted_file_content)?;
 
-    println!("Complete.");
+    println!("AES Encryption Complete.");
 
 
-    Ok(())
+    Ok(key_bytes)
 }
 
-
+// ECC 암호화 함수. AES 비밀 키를 암호화함.
+fn ecc_encryption(key: [u8;KEY_SIZE]) -> Result<(), Box<dyn std::error::Error>> {
+    println!("{:?}", key);
+    Ok(())
+}
