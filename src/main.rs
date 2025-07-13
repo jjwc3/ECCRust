428e0c5486845f7122f4e1d04e7fbf6ffb38d6d7
jjwc3 <155734556+jjwc3@users.noreply.github.com>
2025-07-13 14:10:55 +0900

    250713

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 // Encryption
-// use tokio::fs;
 
-fn main() {
-    let mut file_original_path = String::new();
-    println!("Enter the File Path: ");
-    std::io::stdin().read_line(&mut file_original_path).unwrap();
-    println!("Path: {}", file_original_path);
+#[tokio::main]
+async fn main() -> Result<(), Box<std::io::Error>> {
+    // let mut file_original_path = String::new();
+    // println!("Enter the File Path: ");
+    // std::io::stdin().read_line(&mut file_original_path).unwrap();
+    // println!("Path: {}", file_original_path);
 
-    // let file_path =
+    let file_path = "asdf.txt";
+    let txt = tokio::fs::read_to_string(&file_path).await?;
+    println!("File Contents: {}", txt);
+    Ok(())
 }
 
 
